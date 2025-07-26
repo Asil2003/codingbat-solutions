8c27efc2cbde6ea804cafcbee3c94272f6ce781e
Asil2003 <asilalabri03@gmail.com>
2025-07-26 21:54:43 +0400

    Add solution for warmup1-startOz

---
+++ warmup1/warmup1/StartOz.java
@@ -0,0 +1,18 @@
+public class StartOz {
+    public static String startOz(String str) {
+        String result = "";
+        if (str.length() >= 1 && str.charAt(0) == 'o') {
+            result += 'o';
+        }
+        if (str.length() >= 2 && str.charAt(1) == 'z') {
+            result += 'z';
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(startOz("ozymandias"));  
+        System.out.println(startOz("bzoo"));        
+        System.out.println(startOz("oxx"));         
+    }
+}
