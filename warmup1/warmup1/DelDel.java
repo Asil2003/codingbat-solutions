e23e277c4c0464d86f566247bb18a929f5894098
Asil2003 <asilalabri03@gmail.com>
2025-07-26 21:50:13 +0400

    Add solution for warmup1-delDel

---
+++ warmup1/warmup1/DelDel.java
@@ -0,0 +1,14 @@
+public class DelDel {
+    public static String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+            return str.charAt(0) + str.substring(4);
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(delDel("adelbc"));     
+        System.out.println(delDel("adelHello"));  
+        System.out.println(delDel("adedbc"));     
+    }
+}
