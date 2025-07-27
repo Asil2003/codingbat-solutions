3fa62d17639f1836a94f231112db59ebc27111dd
Asil2003 <asilalabri03@gmail.com>
2025-07-27 10:31:29 +0400

    Add Warmup-2 stringX solution

--- warmup2/Warmup2.java
+++ warmup2/Warmup2.java
@@ -141,3 +141,19 @@ public int stringMatch(String a, String b) {
 }
 
 
+public String stringX(String str) {
+    if (str.length() <= 2) return str;
+
+    StringBuilder result = new StringBuilder();
+    result.append(str.charAt(0)); // keep first character
+
+    for (int i = 1; i < str.length() - 1; i++) {
+        if (str.charAt(i) != 'x') {
+            result.append(str.charAt(i));
+        }
+    }
+
+    result.append(str.charAt(str.length() - 1)); // keep last character
+    return result.toString();
+}
+
