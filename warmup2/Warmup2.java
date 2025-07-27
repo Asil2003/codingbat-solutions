bd1579193a521fa0c5d18906998d93098907eec3
Asil2003 <asilalabri03@gmail.com>
2025-07-27 09:29:58 +0400

    Add Warmup-2 frontTimes solution

--- warmup2/Warmup2.java
+++ warmup2/Warmup2.java
@@ -6,10 +6,22 @@ public class Warmup2 {
         }
         return result;
     }
+    
+
 
     public static void main(String[] args) {
         System.out.println(stringTimes("Hi", 2));  // → "HiHi"
         System.out.println(stringTimes("Hi", 3));  // → "HiHiHi"
         System.out.println(stringTimes("Hi", 1));  // → "Hi"
     }
+}
+
+
+public static String frontTimes(String str, int n) {
+    String front = str.length() < 3 ? str : str.substring(0, 3);
+    String result = "";
+    for (int i = 0; i < n; i++) {
+        result += front;
+    }
+    return result;
 }
