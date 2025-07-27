837cc7e66c745bda4164b579ade69fec0f96fa55
Asil2003 <asilalabri03@gmail.com>
2025-07-27 15:18:29 +0400

    Add String-1 withoutX2 solution

---
+++ string1/withoutX2.java
@@ -0,0 +1,20 @@
+public String withoutX2(String str) {
+    if (str.length() == 0) return "";
+    if (str.length() == 1) {
+        if (str.charAt(0) == 'x') return "";
+        else return str;
+    }
+
+    String result = "";
+    if (str.charAt(0) != 'x') {
+        result += str.charAt(0);
+    }
+    if (str.charAt(1) != 'x') {
+        result += str.charAt(1);
+    }
+    if (str.length() > 2) {
+        result += str.substring(2);
+    }
+
+    return result;
+}
