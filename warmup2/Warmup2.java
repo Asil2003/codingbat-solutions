85d786bd9c5961be6ca62b2844e62c246c6cb4e2
Asil2003 <asilalabri03@gmail.com>
2025-07-27 09:55:34 +0400

    Add Warmup-2 stringBits solution

--- warmup2/Warmup2.java
+++ warmup2/Warmup2.java
@@ -45,3 +45,16 @@ public boolean doubleX(String str) {
 }
 
 
+
+
+
+
+public String stringBits(String str) {
+    String result = "";
+    for (int i = 0; i < str.length(); i += 2) {
+        result += str.charAt(i);
+    }
+    return result;
+}
+
+
