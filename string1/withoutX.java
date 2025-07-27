cb5ef1ba40f75444b4c0c89909fe577ee998bdc5
Asil2003 <asilalabri03@gmail.com>
2025-07-27 15:12:22 +0400

    Add String-1 withoutX solution

---
+++ string1/withoutX.java
@@ -0,0 +1,13 @@
+public String withoutX(String str) {
+    int start = 0, end = str.length();
+
+    if (str.length() > 0 && str.charAt(0) == 'x') {
+        start = 1;
+    }
+
+    if (str.length() > 1 && str.charAt(str.length() - 1) == 'x') {
+        end = str.length() - 1;
+    }
+
+    return str.substring(start, end);
+}
