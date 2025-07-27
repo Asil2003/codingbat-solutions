ced6ab808c2ddf87cac1700bbbb718efd8749598
Asil2003 <asilalabri03@gmail.com>
2025-07-27 10:47:24 +0400

    Add Warmup-2 noTriples solution

--- warmup2/Warmup2.java
+++ warmup2/Warmup2.java
@@ -202,3 +202,12 @@ public int array667(int[] nums) {
 }
 
 
+
+public boolean noTriples(int[] nums) {
+    for (int i = 0; i < nums.length - 2; i++) {
+        if (nums[i] == nums[i+1] && nums[i] == nums[i+2]) {
+            return false;
+        }
+    }
+    return true;
+}
