6b9891a63b42e10842516af74b3b189e8b93cea6
Asil2003 <asilalabri03@gmail.com>
2025-07-27 10:15:49 +0400

    Add Warmup-2 arrayFront9 solution

--- warmup2/Warmup2.java
+++ warmup2/Warmup2.java
@@ -97,3 +97,15 @@ public int arrayCount9(int[] nums) {
 }
 
 
+public boolean arrayFront9(int[] nums) {
+    int end = Math.min(nums.length, 4);
+    for (int i = 0; i < end; i++) {
+        if (nums[i] == 9) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
