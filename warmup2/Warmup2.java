cb3f0eec11c598ce7e4d26a0155b89c67834e290
Asil2003 <asilalabri03@gmail.com>
2025-07-27 10:35:51 +0400

    Add Warmup-2 altPairs solution

--- warmup2/Warmup2.java
+++ warmup2/Warmup2.java
@@ -157,3 +157,17 @@ public String stringX(String str) {
     return result.toString();
 }
 
+
+public String altPairs(String str) {
+    StringBuilder result = new StringBuilder();
+
+    for (int i = 0; i < str.length(); i += 4) {
+        result.append(str.charAt(i)); 
+        if (i + 1 < str.length()) {
+            result.append(str.charAt(i + 1)); 
+        }
+    }
+
+    return result.toString();
+}
+
