6948ae9c3fcc459ff28a4838a3cb669e135069e1
Asil2003 <asilalabri03@gmail.com>
2025-07-30 19:50:02 +0400

    ✅ Add countPairs solution for Recursion-1

---
+++ recursion1/countPairs.java
@@ -0,0 +1,10 @@
+public class countPairs {
+    public static int countPairs(String str) {
+        if (str.length() < 3) return 0;
+        if (str.charAt(0) == str.charAt(2)) {
+            return 1 + countPairs(str.substring(1));
+        } else {
+            return countPairs(str.substring(1));
+        }
+    }
+}
