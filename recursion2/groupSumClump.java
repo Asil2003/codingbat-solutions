340389632a1bbe7657b69ebdb89871d6af6d84c4
Asil2003 <asilalabri03@gmail.com>
2025-07-30 20:15:31 +0400

    ✅ Add groupSumClump solution for Recursion-2

---
+++ recursion2/groupSumClump.java
@@ -0,0 +1,14 @@
+public boolean groupSumClump(int start, int[] nums, int target) {
+    if (start >= nums.length) return target == 0;
+
+    int sum = nums[start];
+    int i = start + 1;
+
+    while (i < nums.length && nums[i] == nums[start]) {
+        sum += nums[i];
+        i++;
+    }
+
+    if (groupSumClump(i, nums, target - sum)) return true;
+    return groupSumClump(i, nums, target);
+}
